64264226ae83a5c74e11d57e4b7b547f06538cf0
Ruiz <IRUIZ1@ball.com>
2025-09-04 15:06:56 -0300

    insert function added

--- src/db.py
+++ src/db.py
@@ -2,32 +2,105 @@ import sqlite3 as sql
 from pathlib import Path
 
 
-class dbIncidents:
+class DbIncidents:
     def __init__(self) -> None:
         self.TABLE_PATH = Path(__file__).parent / "db_IT.db"
         self.TABLE_NAME = "incidents"
 
-    def openConnection(self):
+    def OpenConnection(self):
         self.connection = sql.connect(self.TABLE_PATH)
         self.cursor = self.connection.cursor()
 
-    def closeConnection(self):
+    def CloseConnection(self):
         self.cursor.close()
         self.connection.close()
 
 
-class dbTasks:
+    def InsertName(self, name: str):
+        self.OpenConnection()
+
+        name = f" INSERT INTO {self.TABLE_NAME} (name) VALUES (?)"
+
+        self.cursor.execute(name)
+        self.connection.commit()
+
+        self.CloseConnection()
+
+    def InsertTicketNumber(self, ticket: str):
+        self.OpenConnection()
+
+        ticket = f"INSERT INTO {self.TABLE_NAME} (ticket_number) VALUES (?)"
+
+        self.cursor.execute(ticket)
+        self.connection.commit()
+
+        self.CloseConnection()
+
+    def ReadFile(self, path):
+        with open(path, "rb") as file:
+            return file.read()
+
+    def InsertCheckList(self, CheckList):
+        self.OpenConnection()
+
+        CheckList = self.ReadFile(CheckList)
+
+        self.cursor.execute(f"INSERT INTO {self.TABLE_NAME} (check_list) VALUES (?)", (CheckList))
+        self.connection.commit()
+
+        self.CloseConnection()
+            
+        
+
+
+class DbTasks:
     def __init__(self) -> None:
         self.TABLE_PATH = Path(__file__).parent / "db_IT.db"
         self.TABLE_NAME = "tasks"
 
-    def openConnection(self):
+    def OpenConnection(self):
         self.connection = sql.connect(self.TABLE_PATH)
         self.cursor = self.connection.cursor()
 
-    def closeConnection(self):
+    def CloseConnection(self):
         self.cursor.close()
         self.connection.close()
+
+    def InsertName(self, name: str):
+        self.OpenConnection()
+
+        name = f" INSERT INTO {self.TABLE_NAME} (name) VALUES (?)"
+
+        self.cursor.execute(name)
+        self.connection.commit()
+
+        self.CloseConnection()
+
+    def InsertTicketNumber(self, ticket: str):
+        self.OpenConnection()
+
+        ticket = f"INSERT INTO {self.TABLE_NAME} (ticket_number) VALUES (?)"
+
+        self.cursor.execute(ticket)
+        self.connection.commit()
+
+        self.CloseConnection()
+
+    def ReadFile(self, path):
+        with open(path, "rb") as file:
+            return file.read()
+
+    def InsertCheckList(self, CheckList):
+        self.OpenConnection()
+
+        CheckList = self.ReadFile(CheckList)
+
+        self.cursor.execute(f"INSERT INTO {self.TABLE_NAME} (check_list) VALUES (?)", (CheckList))
+        self.connection.commit()
+
+        self.CloseConnection()
+
+    
 
         
 
